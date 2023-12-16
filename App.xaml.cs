134a448cf2095884dd3b83cd3529196dd541b153
fretka46 <71157224+fretka46@users.noreply.github.com>
2023-12-16 20:52:04 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace First_iOSApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children = { new Label { Text = "Hello World", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center } }
+                }
+            };
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace First_iOSApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
